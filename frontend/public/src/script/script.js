66425fe7e6b3cd7312a4853e69fa48a3d3e4d1fb
calhounbryce13 <uptowncares1@gmail.com>
2026-02-06 23:17:25 -0800

    added the footer element

--- frontend/public/src/script/script.js
+++ frontend/public/src/script/script.js
@@ -1,37 +1,16 @@
-
-
-
 'use strict';
 
 
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
+    toggle_menu_functionality();
+
+});
 
 
-
-
-
-
-
-
-
+const toggle_menu_functionality = function(){
     const menuButton = document.getElementById('menu-button');
     menuButton.addEventListener("click", (event) => {
         console.log("clicking", event.target);
         document.getElementById('options-container').classList.toggle('show-menu');
     });
-
-
-
-
-
-
-
-
-
-
-
-});
+}
