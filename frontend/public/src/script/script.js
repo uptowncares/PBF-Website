3ba86bc962dedd4fac38403596634d45352c7268
calhounbryce13 <uptowncares1@gmail.com>
2026-02-17 22:14:16 -0800

    implemented the more info sections, added the expanding container functionality

--- frontend/public/src/script/script.js
+++ frontend/public/src/script/script.js
@@ -3,8 +3,26 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     toggle_menu_functionality();
+    toggle_more_info_functionality();
 
 });
+
+
+const toggle_more_info_functionality = function(){
+    const buttons = Array.from(document.getElementsByClassName('toggle-control'));
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            const text = event.target.parentNode.parentNode.children[1];
+            if(text.style.height == '0px' || !(text.style.height)){
+                text.style.height = text.scrollHeight + 'px';
+            } 
+            else{
+                text.style.height =  '0px';
+            }
+        });
+    })
+
+}
 
 
 const toggle_menu_functionality = function(){
