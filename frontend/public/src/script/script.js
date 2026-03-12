abf3d737a21f01dc8b512a0a0e7d7a68b34cc213
calhounbryce13 <uptowncares1@gmail.com>
2026-03-11 19:18:23 -0700

    more updates

--- frontend/public/src/script/script.js
+++ frontend/public/src/script/script.js
@@ -5,25 +5,40 @@ document.addEventListener("DOMContentLoaded", () => {
     toggle_menu_functionality();
     toggle_more_info_functionality();
     toggle_about_founder_functionality();
-    //process_volunteer();
-
-
+    page_highlight();
 
 
 });
 
 
-const process_volunteer = function(){
-
-    document.getElementById('submit-volunteer').addEventListener("click", () =>{
-        window.alert("Sorry, we are not able to accept volunteers yet. Please check back later")
-    });
-
+const page_highlight = function(){
+    const pageTitle = document.head.children[0];
+    let index = false;
+    if((pageTitle.textContent).split(" ")[0] == "Home"){
+        index = 0;
+    }
+    else if((pageTitle.textContent).split(" ")[0] == "About"){
+        index = 1;
+    }
+    else if((pageTitle.textContent).split(" ")[0] == "Programs"){
+        index = 4;
+    }
+    else if((pageTitle.textContent).split(" ")[0] == "Donate"){
+        index = 5;
+    }
+    else if((pageTitle.textContent).split(" ")[0] == "Contact"){
+        index = 3;
+    }
+    else if((pageTitle.textContent).split(" ")[0] == "Gallery"){
+        index = 2;
+    }
+    if(index || index == 0){
+        const list = document.getElementById("options-container").children;
+        console.log(list[index]);
+        const element = list[index].children[0];
+        element.classList.add('selected-page-highlight');
+    }
 }
-
-
-
-
 
 
 const toggle_about_founder_functionality = function(){
