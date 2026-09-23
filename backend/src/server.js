60a744803da967d201d46c1decebd66d054ea1ae
Bryce Calhoun <calhounbryce13@gmail.com>
2026-09-23 16:25:40 -0700

    documenting the backend interactions

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,116 +40,110 @@ app.post('/volunteer-registration', async(req, res) => {
         const date = req.body["date"];
         const event = req.body["event"];
         const description = req.body["description"];
-        try{
-            const newVolunteer = await model.add_new_volunteer(name, email, event, date, description);
-            if(newVolunteer){
-                try{
-                    postmarkClient.sendEmail({
 
-                        "From": "pierre@peggybeatricefoundation.org",
-                        "To": email,
-                        "Subject" :"PeggyBeatriceFoundation.org - Thank you for choosing to volunteer with us!" ,
-                        "HtmlBody": `
-                            <table 
-                                width="100%" 
-                                cellpadding="0" 
-                                cellspacing="0" 
-                                border="0"
-                                style="
-                                    margin-top: 20vh;
-                                    background-color: rgba(13, 204, 255, 1);
-                                    color: rgb(0, 0, 0);
-                                    border-radius: 20px;
-                                "
-                            >
-                                <tr>
-                                    <td 
-                                        align="center"
-                                        style="
-                                            padding: 10%;
-                                            text-align: center;
-                                        "
-                                    >
-                                        <h1 style="
-                                            font-size: 3vw;
-                                            margin: 0 0 20px 0;
-                                        ">
-                                            Hi ${name}! Here are your address instructions for getting to us.
-                                        </h1>
-
-                                        <h2 style="
-                                            font-size: 1vw;
-                                            margin: 0 0 20px 0;
-                                        ">
-                                            Just so you remember, you signed up for "${event}" on ${new Date(date).toDateString()}.
-                                        </h2>
-
-                                        <h2 style="
-                                            font-size: 1vw;
-                                            margin: 0;
-                                        ">
-                                            ${utils.addressInstructions[event]}
-                                        </h2>
-                                    </td>
-                                </tr>
-                            </table>
-
-                            <table 
-                                width="100%" 
-                                cellpadding="0" 
-                                cellspacing="0" 
-                                border="0"
-                                style="
-                                    background-color: rgba(231, 22, 197, 1);
-                                    border-radius: 20px
-                                "
-                            >
-                                <tr>
-                                    <td style="
-                                        font-size: 0.75vw;
-                                        padding: 2%;
+        const newVolunteer = await model.add_new_volunteer(name, email, event, date, description);
+        if(newVolunteer){
+            try{
+                postmarkClient.sendEmail({
+                    "From": "pierre@peggybeatricefoundation.org",
+                    "To": email,
+                    "Subject" :"PeggyBeatriceFoundation.org - Thank you for choosing to volunteer with us!" ,
+                    "HtmlBody": `
+                        <table 
+                            width="100%" 
+                            cellpadding="0" 
+                            cellspacing="0" 
+                            border="0"
+                            style="
+                                margin-top: 20vh;
+                                background-color: rgba(13, 204, 255, 1);
+                                color: rgb(0, 0, 0);
+                                border-radius: 20px;
+                            "
+                        >
+                            <tr>
+                                <td 
+                                    align="center"
+                                    style="
+                                        padding: 10%;
+                                        text-align: center;
+                                    "
+                                >
+                                    <h1 style="
+                                        font-size: 3vw;
+                                        margin: 0 0 20px 0;
                                     ">
-                                        &copy; PeggyBeatriceFoundation.org 2026
-                                    </td>
-                                </tr>                                
-                                <tr>
-                                    <td style="
-                                        font-size: 0.75vw;
-                                        padding: 2%;
-                                    ">
-                                    <a href="https://peggybeatricefoundation.org/">
-                                        PeggyBeatriceFoundation.org
-                                    </a>
-                                    </td>
-                                </tr>                                
-                                <tr>
-                                    <td style="
-                                        font-size: 0.75vw;
-                                        padding: 2%;
-                                    ">
-                                        socials
-                                    </td>
-                                </tr>
-                            </table>
-                        `,
-                        "TextBody": "Hello from PBF!",
-                        "MessageStream": "Outbound"
+                                        Hi ${name}! Here are your address instructions for getting to us.
+                                    </h1>
 
-                    });
-                    res.status(201).json("success");
-                    console.log(`\nNEW VOLUNTEER: ${name} for ${event} on ${date}`);
-                }catch(error){
-                    console.error(error);
-                    await model.remove_volunteer(name, email, event, date);
-                    res.status(500).json({"error": "unable to send confirmation email to volunteer"})
-                }
-                return;
+                                    <h2 style="
+                                        font-size: 1vw;
+                                        margin: 0 0 20px 0;
+                                    ">
+                                        Just so you remember, you signed up for "${event}" on ${new Date(date).toDateString()}.
+                                    </h2>
+
+                                    <h2 style="
+                                        font-size: 1vw;
+                                        margin: 0;
+                                    ">
+                                        ${utils.addressInstructions[event]}
+                                    </h2>
+                                </td>
+                            </tr>
+                        </table>
+
+                        <table 
+                            width="100%" 
+                            cellpadding="0" 
+                            cellspacing="0" 
+                            border="0"
+                            style="
+                                background-color: rgba(231, 22, 197, 1);
+                                border-radius: 20px
+                            "
+                        >
+                            <tr>
+                                <td style="
+                                    font-size: 0.75vw;
+                                    padding: 2%;
+                                ">
+                                    &copy; PeggyBeatriceFoundation.org 2026
+                                </td>
+                            </tr>                                
+                            <tr>
+                                <td style="
+                                    font-size: 0.75vw;
+                                    padding: 2%;
+                                ">
+                                <a href="https://peggybeatricefoundation.org/">
+                                    PeggyBeatriceFoundation.org
+                                </a>
+                                </td>
+                            </tr>                                
+                            <tr>
+                                <td style="
+                                    font-size: 0.75vw;
+                                    padding: 2%;
+                                ">
+                                    socials
+                                </td>
+                            </tr>
+                        </table>
+                    `,
+                    "TextBody": "Hello from PBF!",
+                    "MessageStream": "Outbound"
+                });
+                res.status(201).json("success");
+                console.log(`\nNEW VOLUNTEER: ${name} for ${event} on ${date}`);
+            }catch(error){
+                console.error(error);
+                await model.remove_volunteer(name, email, event, date);
+                res.status(500).json({"error": "unable to send confirmation email to volunteer"})
             }
-            res.status(500).json({"error": "model issue registering that volunteer"});
-        }catch(error){
-            console.log(error);
-            res.status(500).json({"error": "server issue communicating with the model"});
+            return;
         }
+        res.status(500).json({"error": "model issue registering that volunteer"});
         return;
     }
     res.status(400).json({"error": "missing data in body"});
